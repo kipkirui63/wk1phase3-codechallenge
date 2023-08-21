b06774253fa67593e54899ffe48c64c59e3a70e0
kipkirui63 <enocksang8356@gmail.com>
2023-08-21 15:53:31 +0300

    updated challenge3

--- challenge3.py
+++ challenge3.py
@@ -1,24 +1,22 @@
-def solution(input_string):
-    # Define the values of consonant characters using a dictionary
-    consonant_values = {'b': 2, 'c': 3, 'd': 4, 'f': 6, 'g': 7, 'h': 8, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14,
-                        'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'v': 21, 'w': 23, 'x': 24, 'y': 25, 'z': 26}
-    
-    current_value = 0  # Initialize the current value
-    max_value = 0  # Initialize the maximum value
-    
-    # Iterate through each character in the input string
-    for character in input_string:
-        if character in consonant_values:  # Check if the character is a consonant
-            current_value += consonant_values[character]  # Add the value of the consonant
-        else:
-            # Update the maximum value and reset the current value
-            max_value = max(max_value, current_value)
-            current_value = 0  # Reset the current value to 0
-    
-    # Update the maximum value after the loop and return it
-    max_value = max(max_value, current_value)
-    return max_value
+def two_positive_integers(a, b, c):# This function takes three integer arguments (a, b, c) and checks if exactly two of them are positive.
 
-# Test cases
-print(solution("strength"))   # Output: 8
-print(solution("software"))   # Output: 19
+    positive_count = 0  # Initialize the count of positive numbers below
+
+    if a > 0:
+        positive_count += 1  # Increment count if a is positive
+    if b > 0:
+        positive_count += 1  # Increment count if b is positive
+    if c > 0:
+        positive_count += 1  # Increment count if c is positive
+
+    return positive_count == 2  # The function returns True if exactly two numbers above are positive, and False if otherwise.
+
+#input the numbers and store them in their respective variables
+a = int(input("Enter the first integer: "))
+b = int(input("Enter the second integer: "))
+c = int(input("Enter the third integer: "))
+
+# Calls two_positive_integers function with the provided inputs and stores the result in the result variable.
+result = two_positive_integers(a, b, c)
+
+print(f"({a}, {b}, {c}) == {result}")# Print the result in the format "(a, b, c) == result"
